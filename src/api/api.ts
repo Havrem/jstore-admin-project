ffc5be0d0309d089bd58e293a4f6cd52f98a8ec1
Havrem <a.lindh@live.se>
2025-10-07 21:55:15 +0200

    Update url

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:8080', // Api address
+  baseURL: 'https://jjfakestore-fork-alex-production.up.railway.app', // Api address
   withCredentials: true, // send/receive JSESSIONID cookie
 })
